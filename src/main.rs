290c2418d6227600214149c69d2c1d24543abe21
= <=>
2023-12-17 20:24:31 -0600

    Convertir string a int

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ fn main() {
     println!("Por favor introduce tu nombre: ");
 
     let mut nombre: String = String::new(); // Formas mas complejas, trim
-    let mut apellido: &str; // Sirve mara mostrar texto, algo más sencillo
-
     std::io::stdin().read_line(&mut nombre).unwrap();
 
-    println!("Hola bienvenido {}", nombre);
+    nombre = nombre.trim().to_string();
+
+    // Obtener la edad de la consola
+    let mut edad : String = String::new();
+    println!("Por favor introduce tu edad: ");
+    std::io::stdin().read_line(&mut edad).unwrap();
+
+    // Convertir esa edad a un numero
+    let edad_int : u8 = edad.trim().parse().unwrap();
+
+    println!("Hola bienvenido {} de {} años", nombre, edad_int);
 }
